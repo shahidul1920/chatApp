4a0335549619b8963bf95f569bd6705c6d93280f
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-01-09 00:06:26 +0600

    sidebar user filter

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,13 @@
-
+import User from "../models/user.model";
 
 export const getUserForSidebar = async (req, res) =>{
-    
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password");
+
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.log("Error in users sidebar :", error.message);
+        res.status(500).json({error: "Server fucked up"})
+    }
 }
