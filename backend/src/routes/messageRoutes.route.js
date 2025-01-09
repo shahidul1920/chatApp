52686605777daa958f05b38f52dfcc78a1b7a595
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-01-09 11:54:36 +0600

    some bugs fixed

--- backend/src/routes/messageRoutes.route.js
+++ backend/src/routes/messageRoutes.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware";
+import { protectRoute } from "../middleware/auth.middleware.js";
 import { getMessages, getUserForSidebar, sendMessages } from "../controllers/message.controller.js";
 
 
